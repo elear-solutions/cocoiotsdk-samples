0f0eb4907a1e731ab8eb4519cd49ec7325ead3b6
shrinivas-elear <53552331+shrinivas-elear@users.noreply.github.com>
2022-02-23 20:54:16 +0530

    Revert "Adds IOTClientSDK Sample App for iOS"
    This reverts commit 42e00be20f6b108f91ac72f00cae4a29d55ce50d.

--- client/ios/Pods/Target Support Files/Pods-IoTSample-App-IoTSample-AppUITests/Pods-IoTSample-App-IoTSample-AppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IoTSample_App_IoTSample_AppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IoTSample_App_IoTSample_AppUITests
-@end
--- client/ios/Pods/Target Support Files/Pods-IoTSample-App/Pods-IoTSample-App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IoTSample_App : NSObject
-@end
-@implementation PodsDummy_Pods_IoTSample_App
-@end
